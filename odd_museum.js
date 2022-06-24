940f0738b6879d10126a3fc992482ffe4a62990a
toripppppy <Lundy11@icloud.com>
2022-06-24 11:49:45 +0900

    Update odd_museum.js
    作品解説を追加

--- odd_museum.js
+++ odd_museum.js
@@ -3,13 +3,12 @@
 /*
     Welcome to Odd Museum!
 
-    奇数を導く、そのソースコードの美しさを体感しよう！
-    ※敬称略
+    奇数を導く、そのソースコードの美しさを体感しよう...
 */
 
 
-//すべての始まり、それは偶数と奇数であろう
-//偶数
+//すべての始まり、それは偶数と奇数であろう。
+//偶数・奇数
 let n = 30, m = 31;
 //初期化用
 const init = () => {
@@ -18,12 +17,14 @@ const init = () => {
 }
 
 
-/*--本館--*/
+
+/*-- 本館 --*/
 
 /* 安心と信頼のif文 */
 init()
 
 const commonIf = (x) => {
+
     if (x % 2 === 0) { //偶数なら
         x++;
     }
@@ -34,6 +35,10 @@ console.log( commonIf(n) )
 //31
 console.log( commonIf(m) )
 //31
+/*
+if文を使った一番典型的な形。
+見慣れていること、またその視認性の良さからとても安心できるコード。
+*/
 
 
 
@@ -41,18 +46,24 @@ console.log( commonIf(m) )
 init()
 
 const worrierIf = (x) => {
+
     if (x % 2 === 0) { //偶数なら
         x++;
     } else {
         x = x; //念のため
     }
     return x;
+
 }
 
 console.log( worrierIf(n) )
 //31
 console.log( worrierIf(m) )
 //31
+/*
+普通のif文にelseを加えて念押しの処理を足した形。
+安心のためにコードを余分に書くのが特徴。
+*/
 
 
 
@@ -60,15 +71,21 @@ console.log( worrierIf(m) )
 init()
 
 const smartIf = (x) => {
+
     if (x % 2 === 0) x++;
 
     return x;
+
 }
 
 console.log( smartIf(n) )
 //31
 console.log( smartIf(m) )
 //31
+/*
+一行に収まったことでスタイリッシュな印象を与えるif文。
+安心感、コンパクトさの二点で非常に優れている。
+*/
 
 
 
@@ -76,59 +93,79 @@ console.log( smartIf(m) )
 init()
 
 const addOdd = (x) => {
+
     return x + 1 - (x % 2);
+
 }
 
 console.log( addOdd(n) )
 //31
 console.log( addOdd(m) )
 //31
+/*
+if文を使わずに代入式に落とし込んだ形。
+コンパクトな反面、処理を直感的に認識できないのが難点。
+*/
 
 
 
-/* 三項演算子 */
-
-//from muguet
+/* 三項演算子 */ //muguet様より寄贈
 init()
 
-const ternaryIf = (x) => {
-    return (x % 2 === 0) ? x + 1 : x;
+const ternaryOp = (x) => {
+
+    return x % 2 === 0 ? x + 1 : x;
+
 }
 
-console.log( ternaryIf(n) )
+console.log( ternaryOp(n) )
 //31
-console.log( ternaryIf(m) )
+console.log( ternaryOp(m) )
 //31
+/*
+三項演算子を使った見慣れない形が特徴。
+ifelse文と同じ処理が行われているため、安心感がある。
+*/
 
 
 
-/* 三項演算子 boolタイプ */
-
-//from chabo
+/* 三項演算子 boolタイプ */ //chabo様より寄贈
 init()
 
-const boolTernaryIf = (x) => {
+const boolTernaryOp = (x) => {
+
     return Boolean(x % 2) ? x : x + 1;
+
 }
 
-console.log( boolTernaryIf(n) )
+console.log( boolTernaryOp(n) )
 //31
-console.log( boolTernaryIf(m) )
+console.log( boolTernaryOp(m) )
 //31
+/*
+三項演算子とboolを組み合わせて複雑化。
+その色とりどりなコードから、他とは違う優越感が感じられる。
+*/
 
 
 
-/* ビット演算 */
+/* ビット演算 */ //hachitai様より寄贈
 
 //最短賞
-//from hachitai
 init()
 
-const bitwiseOp = (x) => {
+const bitwiseOp = (x) => { // bitwise operation
+
     return x | 1;
+
 }
 
 console.log( bitwiseOp(n) )
 //31
 console.log( bitwiseOp(m) )
 //31
+/*
+管理人イチオシ
+美術館最短コードであり、その処理のスタイリッシュさに思わず度肝を抜かれる。
+二進数にした際の一番右の位を強制的に１にすることで奇数に変換。圧巻。理論値。
+*/
